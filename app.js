54ebd0f33f4e8e85cf30ce0ed0a8d726f2041207
Krishnan Saravanan <skrishnan0148@gmail.com>
2023-07-21 04:50:14 +0530

    third commit

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const itemsSchema ={
 const Item= mongoose.model("Item",itemsSchema);
 
 const item1 = new Item({
-  name:"Buy groceries"
+  name:"Proper Sleep"
 });
 const item2 = new Item({
   name:"Buy pens"
