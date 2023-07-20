abb23753d1c3a335eb37a5dbca2e54d9f41efa02
Krishnan Saravanan <skrishnan0148@gmail.com>
2023-07-21 04:28:46 +0530

    second commit

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const item3 = new Item({
   name:"Buy boots"
 })
 
-const defaultItems =[item1,item2,item3];
+const defaultItems =[];
 
 const listSchema = {
   name : String,
@@ -134,6 +134,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT ||3000, function() {
   console.log("Server started on port 3000");
 });
